0966f5190c843773dd17931301bff3d2f2e0078a
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-03-31 21:56:28 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,23 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "KhKMyvcsLsLPHkb7EdddjKR1FgJ5FEkGZe77t1BkJkc",
-        spaceId: "bvdghcc7jm44",
+        accessToken: "",
+        spaceId: "",
       },
     },
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-smoothscroll",
+    "gatsby-image",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/data/images`,
+      },
+    },
   ],
 };
